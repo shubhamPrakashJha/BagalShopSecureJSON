4cba103a7cbaa053381a7296dc916d30e5c9a782
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-31 13:13:30 +0530

    minor fix

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from models import Base, User, Bagel
+from database_setup import Base, Bagel
 from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
